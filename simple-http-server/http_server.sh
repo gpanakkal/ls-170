ba5206475ea54c5b98edf6d753a5a7874f4c50f1
Gautam Panakkal <gautampanakkal@gmail.com>
2024-05-10 01:45:01 -0700

    working on server

--- simple-http-server/http_server.sh
+++ simple-http-server/http_server.sh
@@ -3,12 +3,29 @@
 function server() {
   while true
   do
-    read method path version
+    message_arr=()
+    check=true
+    while $check
+    do
+      read line
+      message_arr+=($line)
+      if [[ "${#line}" -eq 1 ]]
+      then
+        check=false
+      fi
+    done
+    method=${message_arr[0]}
+    path=${message_arr[1]}
     if [[ $method = 'GET' ]]
     then
-      echo HTTP/1.1 200 OK
+      if [[ -f "./www/$path" ]]
+      then
+        echo -ne 'HTTP/1.1 200 OK\r\n\r\n'; cat "./www/$path"
+      else
+        echo -ne 'HTTP/1.1 404 Not Found\r\n\r\n'
+      fi
     else
-      echo HTTP 1.1 400 Bad Request
+      echo -ne 'HTTP 1.1 400 Bad Request\r\n\r\n'
     fi
   done
 }
